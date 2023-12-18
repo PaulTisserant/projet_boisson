626923c4f4539dc6aaa0c28860ecf623fd409c1c
Paul Tisserant <paul2.tisserant@gmx.fr>
2023-12-18 13:34:15 +0100

    bdd + overall display

---
+++ bdd.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 16 déc. 2023 à 20:42
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `boissons`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `favoriterecipes`
+--
+
+CREATE TABLE `favoriterecipes` (
+  `user_id` int(11) NOT NULL,
+  `recipe_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `foodhierarchy`
+--
+
+CREATE TABLE `foodhierarchy` (
+  `food_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `hierarchyrelationships`
+--
+
+CREATE TABLE `hierarchyrelationships` (
+  `parent_food_id` int(11) NOT NULL,
+  `child_food_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `ingredient_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recipeingredients`
+--
+
+CREATE TABLE `recipeingredients` (
+  `recipe_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  `quantity` varchar(50) DEFAULT NULL,
+  `unit` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recipes`
+--
+
+CREATE TABLE `recipes` (
+  `recipe_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `preparation` text DEFAULT NULL,
+  `photo_path` varchar(255) DEFAULT NULL,
+  `ingredients` text DEFAULT NULL,
+  `food_index` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `postal_code` varchar(10) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `favoriterecipes`
+--
+ALTER TABLE `favoriterecipes`
+  ADD PRIMARY KEY (`user_id`,`recipe_id`),
+  ADD KEY `recipe_id` (`recipe_id`);
+
+--
+-- Index pour la table `foodhierarchy`
+--
+ALTER TABLE `foodhierarchy`
+  ADD PRIMARY KEY (`food_id`);
+
+--
+-- Index pour la table `hierarchyrelationships`
+--
+ALTER TABLE `hierarchyrelationships`
+  ADD PRIMARY KEY (`parent_food_id`,`child_food_id`),
+  ADD KEY `child_food_id` (`child_food_id`);
+
+--
+-- Index pour la table `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`ingredient_id`);
+
+--
+-- Index pour la table `recipeingredients`
+--
+ALTER TABLE `recipeingredients`
+  ADD PRIMARY KEY (`recipe_id`,`ingredient_id`),
+  ADD KEY `ingredient_id` (`ingredient_id`);
+
+--
+-- Index pour la table `recipes`
+--
+ALTER TABLE `recipes`
+  ADD PRIMARY KEY (`recipe_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `foodhierarchy`
+--
+ALTER TABLE `foodhierarchy`
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ingredients`
+--
+ALTER TABLE `ingredients`
+  MODIFY `ingredient_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `recipes`
+--
+ALTER TABLE `recipes`
+  MODIFY `recipe_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `favoriterecipes`
+--
+ALTER TABLE `favoriterecipes`
+  ADD CONSTRAINT `favoriterecipes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `favoriterecipes_ibfk_2` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`);
+
+--
+-- Contraintes pour la table `hierarchyrelationships`
+--
+ALTER TABLE `hierarchyrelationships`
+  ADD CONSTRAINT `hierarchyrelationships_ibfk_1` FOREIGN KEY (`parent_food_id`) REFERENCES `foodhierarchy` (`food_id`),
+  ADD CONSTRAINT `hierarchyrelationships_ibfk_2` FOREIGN KEY (`child_food_id`) REFERENCES `foodhierarchy` (`food_id`);
+
+--
+-- Contraintes pour la table `recipeingredients`
+--
+ALTER TABLE `recipeingredients`
+  ADD CONSTRAINT `recipeingredients_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`),
+  ADD CONSTRAINT `recipeingredients_ibfk_2` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- insertion.php
+++ insertion.php
@@ -9,69 +9,86 @@
     <h1>Insertion script</h1>
     <?php
     include "Donnees.inc.php";
-    //include "connect.php";
 
-    /**
-    * Ingredient = (id_ing VARCHAR(50), nom VARCHAR(50));
-    * Recette = (id_rec VARCHAR(50), nom_rec VARCHAR(50), preparation VARCHAR(500));
-    * SousCategorie = (id_sousCat VARCHAR(50), nom_sousCat VARCHAR(50));
-    * SuperCategorie = (id_supCat VARCHAR(50), nom_supCat VARCHAR(50));
-    * Quantite = (id_quant VARCHAR(50), quantite VARCHAR(50), #id_ing);
-    * rangeDansSupCat = (#id_sousCat, #id_supCat);
-    * Composee = (#id_ing, #id_rec);
-    * rangeDansSousCat = (#id_ing, #id_sousCat);
-    */
 
-    try {
-        $conn = new PDO('mysql:host=localhost;dbname=boissons', "root", "");
-        foreach($Hierarchie as $sousCateg => $value){
-            $conteur1 = 1;
-            echo "<h2>$sousCateg</h2>";
-            
-            $nom_sousCat = $sousCateg;
-            // insert into SousCategorie values (1, 'test');
-            $sql = "insert into SousCategorie values ($conteur1, '$nom_sousCat');";
-            $conn->exec($sql);
-            
-    
-            foreach($value as $superCateg => $value2){
-                $conteur2 = 1;
-                echo "<h3>$superCateg</h3>";
-                
-                $nom_supCat = $value2;
-                // insert into SuperCategorie values (1, 'test');
-                $sql = "insert into SuperCategorie values ($conteur2, '$value2');";
-                $conn->exec($sql);
-                
-                echo "<ul>";
-    
-                foreach($value2 as $ingredients){
-                    $conteur3 = 1;
-                    echo "<li>$ingredients</li>";
-                    
-                    $nom_rec = $ingredients;
-                    // insert into Ingredient values (1, 'test');
-                    $sql = "insert into Ingredient values ($conteur3, '$nom_rec');";
-                    $conn->exec($sql);
-                    
-                    $conteur3++;
-                }
-                echo "</ul>";
-                $conteur2++;
-            }
-            $conteur1++;
+  // Fonction pour obtenir l'ID d'un aliment dans la table FoodHierarchy
+  function getFoodId($foodName, $conn)
+  {
+      $sql = "SELECT food_id FROM FoodHierarchy WHERE name = :foodName";
+      $stmt = $conn->prepare($sql);
+      $stmt->bindParam(':foodName', $foodName, PDO::PARAM_STR);
+      $stmt->execute();
+
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      if ($result) {
+          return $result['food_id'];
+      } else {
+          // Insérer l'aliment s'il n'existe pas encore
+          $sql = "INSERT INTO FoodHierarchy (name) VALUES (:foodName)";
+          $stmt = $conn->prepare($sql);
+          $stmt->bindParam(':foodName', $foodName, PDO::PARAM_STR);
+          $stmt->execute();
+          return $conn->lastInsertId();
+      }
+  }
+
+  try {
+    $conn = new PDO('mysql:host=localhost;dbname=boissons', "root", "");
+
+  // Insertion des données de la hiérarchie des aliments
+  foreach ($Hierarchie as $categoryName => $categoryData) {
+    if (isset($categoryData['sous-categorie']) && is_array($categoryData['sous-categorie'])) {
+        foreach ($categoryData['sous-categorie'] as $subCategoryName) {
+            $parentId = getFoodId($categoryName, $conn);
+            $childId = getFoodId($subCategoryName, $conn);
+
+            // Insérer la relation de hiérarchie
+            $sql = "INSERT INTO HierarchyRelationships (parent_food_id, child_food_id) VALUES (?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$parentId, $childId]);
         }
-    } catch (PDOException $e) {
-        // tenter de réessayer la connexion après un certain délai, par exemple
-        echo "Erreur !: " . $e->getMessage() . "<br/>";
     }
+  }
 
 
+  // Insertion des données des recettes
+  foreach ($Recettes as $recipe) {
+    $title = $recipe['titre'];
+    $ingredients = $recipe['ingredients'];
+    $preparation = $recipe['preparation'];
+    $index = implode(', ', $recipe['index']); // Concaténez les éléments de l'index
+
+    // Insérer la recette
+    $sql = "INSERT INTO Recipes (title, ingredients, preparation, food_index) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$title, $ingredients, $preparation, $index]);
+
+    // Récupérer l'ID de la recette insérée
+    $recipeId = $conn->lastInsertId();
+
+    // Vérifier si la recette a une photo
+    if (isset($recipe['photo'])) {
+        $photo = $recipe['photo'];
+
+        // Insérer la photo de la recette
+        $sql = "INSERT INTO RecipePhotos (recipe_id, photo) VALUES (?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$recipeId, $photo]);
+    }
+}
 
 
+} catch (PDOException $e) {
+    // tenter de réessayer la connexion après un certain délai, par exemple
+    echo "Erreur !: " . $e->getMessage() . "<br/>";
 
+} finally {
+    $conn = null;
+}
 
-    ?>
+?>
+
 </body>
 </html>
 <!DOCTYPE html>
