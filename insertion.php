5cce9eec1617a170bbcde7d6194e433a359f9305
Paul Tisserant <paul2.tisserant@gmx.fr>
2023-12-18 18:51:11 +0100

    add bdd initializator

--- insertion.php
+++ insertion.php
@@ -36,56 +36,72 @@
   try {
     $conn = new PDO('mysql:host=localhost;dbname=boissons', "root", "");
 
-  // Insertion des données de la hiérarchie des aliments
-  foreach ($Hierarchie as $categoryName => $categoryData) {
-    if (isset($categoryData['sous-categorie']) && is_array($categoryData['sous-categorie'])) {
-        foreach ($categoryData['sous-categorie'] as $subCategoryName) {
-            $parentId = getFoodId($categoryName, $conn);
-            $childId = getFoodId($subCategoryName, $conn);
+    // Insertion des données de la hiérarchie des aliments
+    foreach ($Hierarchie as $categoryName => $categoryData) {
+        if (isset($categoryData['sous-categorie']) && is_array($categoryData['sous-categorie'])) {
+            foreach ($categoryData['sous-categorie'] as $subCategoryName) {
+                $parentId = getFoodId($categoryName, $conn);
+                $childId = getFoodId($subCategoryName, $conn);
 
-            // Insérer la relation de hiérarchie
-            $sql = "INSERT INTO HierarchyRelationships (parent_food_id, child_food_id) VALUES (?, ?)";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute([$parentId, $childId]);
+                // Insérer la relation de hiérarchie
+                $sql = "INSERT INTO HierarchyRelationships (parent_food_id, child_food_id) VALUES (?, ?)";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute([$parentId, $childId]);
+            }
         }
     }
-  }
 
 
-  // Insertion des données des recettes
-  foreach ($Recettes as $recipe) {
-    $title = $recipe['titre'];
-    $ingredients = $recipe['ingredients'];
-    $preparation = $recipe['preparation'];
-    $index = implode(', ', $recipe['index']); // Concaténez les éléments de l'index
+    // Insertion des données des recettes
+    foreach ($Recettes as $recipe) {
+        $title = $recipe['titre'];
+        $ingredients = $recipe['ingredients'];
+        $preparation = $recipe['preparation'];
+        $index = implode(', ', $recipe['index']); // Concaténez les éléments de l'index
 
-    // Insérer la recette
-    $sql = "INSERT INTO Recipes (title, ingredients, preparation, food_index) VALUES (?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$title, $ingredients, $preparation, $index]);
-
-    // Récupérer l'ID de la recette insérée
-    $recipeId = $conn->lastInsertId();
-
-    // Vérifier si la recette a une photo
-    if (isset($recipe['photo'])) {
-        $photo = $recipe['photo'];
-
-        // Insérer la photo de la recette
-        $sql = "INSERT INTO RecipePhotos (recipe_id, photo) VALUES (?, ?)";
+        // Insérer la recette
+        $sql = "INSERT INTO Recipes (title, ingredients, preparation, food_index) VALUES (?, ?, ?, ?)";
         $stmt = $conn->prepare($sql);
-        $stmt->execute([$recipeId, $photo]);
+        $stmt->execute([$title, $ingredients, $preparation, $index]);
+
     }
-}
+
+    $sql = "SELECT title, food_index, ingredients, preparation FROM recipes";
+    $result = $conn->query($sql);
+    $result->setFetchMode(PDO::FETCH_ASSOC);
+
+    // Pour chaque recette
+    while ($row = $result->fetch()) {
+        // Afficher le nom de la recette
+        $ingredients = explode(",", $row['food_index']);
+        foreach ($ingredients as $ingredient) {
+            $sql2 = "SELECT nom from ingredients where nom = :ingredient";
+            $stmt2 = $conn->prepare($sql2);
+            $stmt2->bindParam(':ingredient', $ingredient, PDO::PARAM_STR);
+            $stmt2->execute();
+            $result2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+            if ($result2) {
+                echo "L'ingrédient ".$ingredient." existe déjà dans la base de données";
+            } else {
+                $sql3 = "INSERT INTO ingredients (nom) VALUES (:ingredient)";
+                $stmt3 = $conn->prepare($sql3);
+                $stmt3->bindParam(':ingredient', $ingredient, PDO::PARAM_STR);
+                $stmt3->execute();
+                echo "L'ingrédient ".$ingredient." a été ajouté à la base de données";
+            }
+        }
+
+    }
 
 
-} catch (PDOException $e) {
-    // tenter de réessayer la connexion après un certain délai, par exemple
-    echo "Erreur !: " . $e->getMessage() . "<br/>";
 
-} finally {
-    $conn = null;
-}
+    } catch (PDOException $e) {
+        // tenter de réessayer la connexion après un certain délai, par exemple
+        echo "Erreur !: " . $e->getMessage() . "<br/>";
+
+    } finally {
+        $conn = null;
+    }
 
 ?>
 
