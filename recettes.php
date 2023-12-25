d10b8ff9343d77a5411c741c023c5dcc0324792e
Paul Tisserant <paul2.tisserant@gmx.fr>
2023-12-25 19:28:01 +0100

    ajout séléction par ingredients, ajustements research bar et ajout d'une photo par défault

--- recettes.php
+++ recettes.php
@@ -26,44 +26,77 @@
     <form action="recettes.php" method="GET">
         <input type="text" name="search" placeholder="Rechercher une recette" class="searchBar">
         <input type="submit" value="Rechercher" class="researchButton">
+        <?php 
+            //recherche par ingredients avec un selecteur
+
+            try {
+                $conn = new PDO('mysql:host=localhost;dbname=boissons', "root", "");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $stmt = $conn->prepare("SELECT * FROM ingredients");
+                $stmt->execute();
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $ingredients = "";
+
+                if (isset($_GET['ingredients']) && $_GET['ingredients'] != "") {
+                    $ingredients = $_GET['ingredients'];
+                }
+
+                echo "<select name='ingredients'>";
+                echo "<option value='$ingredients'>Rechercher par ingrédients</option>";
+                foreach ($result as $ingredient) {
+                    echo "<option value='" . $ingredient['nom'] . "'>" . $ingredient['nom'] . "</option>";
+                }
+                echo "</select>";
+
+            } catch (PDOException $e) {
+                echo "Erreur : " . $e->getMessage();
+            }
+        ?>
     </form>
 
-    <?php
-    
-        try {
-            $conn = new PDO('mysql:host=localhost;dbname=boissons', "root", "");
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    <div class="cardContainer">
 
-            // Recherche par nom de recette
-            if (isset($_GET['search'])) {
-                $search = $_GET['search'];
-                $sql = "SELECT * FROM Recipes WHERE title LIKE :search";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindValue(':search', "%$search%", PDO::PARAM_STR);
-            } else {
-                $sql = "SELECT * FROM Recipes";
-                $stmt = $conn->prepare($sql);
+        <?php
+        
+            try {
+                $conn = new PDO('mysql:host=localhost;dbname=boissons', "root", "");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // recherche si la variable search se trouve dans toutes les données d'une recette
+                if (isset($_GET['search']) && $_GET['search'] != "") {
+                    $search = $_GET['search'];
+                    $stmt = $conn->prepare("SELECT * FROM recipes WHERE title LIKE '%$search%' OR food_index LIKE '%$search%' OR ingredients LIKE '%$search%' OR preparation LIKE '%$search%'");
+                } else if (isset($_GET['ingredients'])) {
+                    $stmt = $conn->prepare("SELECT * FROM recipes WHERE food_index LIKE '%" . $_GET['ingredients'] . "%'");
+                } else {
+                    $stmt = $conn->prepare("SELECT * FROM recipes");
+                }
+
+                $stmt->execute();
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($result as $recipe) {
+                    echo "<div class='card'>";
+                    echo "<h2>" . $recipe['title'] . "</h2>";
+                    echo "<img src='Photos/default.png' alt='" . $recipe['title'] . "'>";
+                    echo "<p>" . $recipe['food_index'] . "</p>";
+                    echo "</div>";
+                }
+
+                
+
+                
+            } catch (PDOException $e) {
+                echo "Erreur : " . $e->getMessage();
             }
 
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            foreach ($result as $recipe) {
-                // faire une carte avec le titre de la recette, les ingrédients et la photo
-                echo "<div class='card'>";
-                echo "<h2>" . $recipe['title'] . "</h2>";
-                echo "<img src='images/" . $recipe['title'] . "' alt='" . $recipe['title'] . "'>";
-                echo "<p>" . $recipe['food_index'] . "</p>";
-                echo "</div>";
-            }
-
-            
-        } catch (PDOException $e) {
-            echo "Erreur : " . $e->getMessage();
-        }
 
 
-    ?>
+
+        ?>
+
+    </div>
 
     
 </body>
