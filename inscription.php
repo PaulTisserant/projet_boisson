1a0a17a89c6b2cf0b155859c5e107e79c7db157e
Mans Leo <leo.mans8@etu.univ-lorraine.fr>
2023-12-27 23:00:52 +0100

    Ajout debut JS Inscription

--- inscription.php
+++ inscription.php
@@ -6,224 +6,228 @@
     <title>Inscription</title>
 </head>
 <body>
+<script>
+        var label_nom = document.createElement("label");
+        label_nom.textContent = "(Erreur Le nom est obligatoire)" ;
+        label_nom.style.fontSize = "10px" ;
+        label_nom.style.color = "red" ;
 
-<?php  
-// definition des varaiables
-$prenom = TRUE ;
-$nom = TRUE ;
-$email = TRUE ;
-$sexe = TRUE ;
-$ville = TRUE ;
-$codePostal = TRUE ;
-$adresse = TRUE ;
-$naissance = TRUE ;
-$motDePasse = TRUE ;
-$login = TRUE ;
-$telephone = TRUE ;
-$mdp_SizeError = FALSE ;
-$login_SizeError = FALSE ;
-$login_ExistError = FALSE ;
+        var label_prenom = document.createElement("label");
 
-$mdp_EqualsError = FALSE ;
-$mdp_UpperError = FALSE ;
+        label_prenom.textContent = "(Erreur Le prenom est obligatoire)" ;
+        label_prenom.style.fontSize = "10px" ;
+        label_prenom.style.color = "red" ;
 
-//Verification des donnees
-if($_POST['Submit']){
-    //Verif NOM
-    if(isset($_POST['Nom'])){
-         $pre = $_POST['Nom'] ;
-         //Verification si l'utiliateur a remplis le champ Nom
-        if(strlen($pre) > 0 ){
-            if(preg_match('/\d/',$pre)){
-                $nom = FALSE ;
+        var label_email = document.createElement("label");
+        label_email.textContent = "(Erreur Email incorrect)" ;
+        label_email.style.fontSize = "10px" ;
+        label_email.style.color = "red" ;
+
+        label_tel = document.createElement("label");
+        label_tel.textContent = "(Erreur Le nom est obligatoire)" ;
+        label_tel.style.fontSize = "10px" ;
+        label_tel.style.color = "red" ;
+
+        label_ville = document.createElement("label");
+        label_ville.textContent = "(Erreur Le nom est obligatoire)" ;
+        label_ville.style.fontSize = "10px" ;
+        label_ville.style.color = "red" ;
+
+        label_codePostal = document.createElement("label");
+        label_codePostal.textContent = "(Erreur Le nom est obligatoire)" ;
+        label_codePostal.style.fontSize = "10px" ;
+        label_codePostal.style.color = "red" ;
+
+        label_login = document.createElement("label");
+        label_login.textContent = "(Erreur Le nom est obligatoire)" ;
+        label_login.style.fontSize = "10px" ;
+        label_login.style.color = "red" ;
+
+        label_mdp = document.createElement("label");
+        label_mdp.textContent = "(Erreur Le nom est obligatoire)" ;
+        label_mdp.style.fontSize = "10px" ;
+        label_mdp.style.color = "red" ;
+
+    function handleBlurNom() {
+        var nom_input = document.getElementById("nom_input");
+        var nom = document.getElementById("nom") ;
+        if ( /\d/.test(nom_input.value) ) {
+            if(!nom.contains(label_nom)){
+                nom.appendChild(label_nom) ;
             }
-        }
-    }
-    //Verif PRENOM
-    if(isset($_POST['Prenom'])){
-        $pre = $_POST['Prenom'] ;
-        //Verification si l'utiliateur a remplis le champ Prenom
-       if(strlen($pre) > 0 ){
-           if(preg_match('/\d/',$pre)){
-               $prenom = FALSE ;
-           }
-       }
-    }
-    //Verif EMAIL
-    if(isset($_POST['email'])){
-        $mail = $_POST['email'] ;
-        //Verification si l'utiliateur a remplis le champ Email
-       if(preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i',$var_mail ))
-       {
-            $email = FALSE ;
-       }
-    }
-    //Verif Telephone
-    if(isset($_POST['telephone'])){
-        $tele = $_POST['telephone'] ;
-        //Verification si l'utiliateur a remplis le champ CodePostal
-        if(strlen($tele) > 0 ){
-            if(!preg_match('/^[0-9]*$/',$tele)){
-                $telephone = FALSE ;
+            nom.style.color = "red" ;      
+        }else{
+            if(nom.contains(label_nom)){
+                nom.removeChild(label_nom) ;
             }
+            nom.style.color = "black" ;
         }
     }
-    //Verif VILLE
-    if(isset($_POST['ville'])){
-        $vil = $_POST['ville'] ;
-        //Verification si l'utiliateur a remplis le champ Villle
-       if(strlen($vil) > 0 ){
-           if(!preg_match('/^[a-zA-Z]*$/',$vil)){
-               $ville = FALSE ;
-           }
-       }
-    }
-    //Verif CodePostal
-    if(isset($_POST['Postal'])){
-        $post = $_POST['Postal'] ;
-        //Verification si l'utiliateur a remplis le champ CodePostal
-        if(strlen($post) > 0 ){
-            if(!preg_match('/^[0-9]*$/',$post)){
-                $codePostal = FALSE ;
+    function handleBlurPrenom() {
+        var prenom_input = document.getElementById("prenom_input");
+        var prenom = document.getElementById("prenom");
+        if ( /\d/.test(prenom_input.value)) {
+            if (!prenom.contains(label_prenom)) {
+                prenom.appendChild(label_prenom);
             }
-        }
-    }
-
-    //Verif Sexe
-    if (isset($_POST['sexe'])) {
-        if (!($_POST['sexe'] ==  'F' || $_POST['sexe'] == 'H')) {
-            $sexe=FALSE ;
-        }
-    }
-    //Verif Date Naissance
-    if (isset($_POST['naissance'])) {
-        $date=$_POST['naissance'] ;
-        if (!(checkdate(date("m",strtotime($date) ), date("d",strtotime($date) ), date("Y",strtotime($date) )))) {
-          $naissance=FALSE ;
-        }
-    }
-
-    //Verif Login
-    if(isset($_POST['Login'])){
-        $log = $_POST['Login'] ;
-        //Verification si l'utiliateur a remplis le champ Login avec au moins 5 caracteres
-        if(!preg_match('/.{5,}/',$log) ){
-                $login = FALSE ;
-                $login_SizeError = TRUE ;
-
-        }
-        //Verification si le login est disponible
-        try {
-            $conn = new PDO('mysql:host=localhost;dbname=boissons', "root", "");
-            
-            // Utilisation de requête préparée pour éviter les attaques par injection SQL
-            $sql = "SELECT user_id FROM users WHERE login = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute([$log]);
-            // Si une ligne est retournée, le login existe
-            if ($stmt->rowCount() > 0) {
-                $login= FALSE ;
-                $login_ExistError = TRUE ;
+            prenom.style.color = "red";
+        } else {
+            if (prenom.contains(label_prenom)) {
+                prenom.removeChild(label_prenom);
             }
-        
-        } catch (PDOException $e) {
-            echo "Erreur : " . $e->getMessage();
-        } finally {
-            $conn = null;
+            prenom.style.color = "black";
+        }
+    }
+    function handleBlurEmail() {
+        var email_input = document.getElementById("email_input");
+        var email = document.getElementById("email");
+
+        if ( /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/.test(email_input.value)) {
+            if (!email.contains(label_email)) {
+                email.appendChild(label_email);
+            }
+            email.style.color = "red";
+        } else {
+            if (email.contains(label_email)) {
+                email.removeChild(label_email);
+            }
+            email.style.color = "black";
+        }
+    }  
+    function handleBlurVille() {
+        var ville_input = document.getElementById("ville_input");
+        var ville = document.getElementById("ville");
+
+        if (! /^[a-zA-Z-]*$/.test(ville_input.value)) {
+            if (!ville.contains(label_ville)) {
+                ville.appendChild(label_ville);
+            }
+            ville.style.color = "red";
+        } else {
+            if (ville.contains(label_ville)) {
+                ville.removeChild(label_ville);
+            }
+            ville.style.color = "black";
+        }
+    } 
+    function handleBlurCodePostal() {
+        var codePostal_input = document.getElementById("codePostal_input");
+        var codePostal = document.getElementById("codePostal");
+
+        if (!/\d{5}/.test(codePostal_input.value)) {
+            if (!codePostal.contains(label_codePostal)) {
+                codePostal.appendChild(label_codePostal);
+            }
+            codePostal.style.color = "red";
+        } else {
+            if (codePostal.contains(label_codePostal)) {
+                codePostal.removeChild(label_codePostal);
+            }
+            codePostal.style.color = "black";
+        }
+    }
+    function handleBlurTel() {
+        var tel_input = document.getElementById("tel_input");
+        var tel = document.getElementById("tel");
+
+        if ( !/^.{0}$/.test(tel_input.value)  && !/(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}/.test(tel_input.value)) {
+            if (!tel.contains(label_tel)) {
+                tel.appendChild(label_tel);
+            }
+            tel.style.color = "red";
+        } else {
+            if (tel.contains(label_tel)) {
+                tel.removeChild(label_tel);
+            }
+            tel.style.color = "black";
+        }
+    }
+    function handleBlurLogin() {
+        var login_input = document.getElementById("login_input");
+        var login = document.getElementById("login");
+
+        if (!/.{5,}/.test(login_input.value)) {
+            if (!login.contains(label_login)) {
+                login.appendChild(label_login);
+            }
+            login.style.color = "red";
+        } else {
+            if (login.contains(label_login)) {
+                login.removeChild(label_login);
+            }
+            login.style.color = "black";
         }
     }    
+    function handleBlurMdp() {
+        var mdp_input = document.getElementById("mdp_input");
+        var mdp_confirm_input = document.getElementById("mdp_confirm_input");
+        var mdp = document.getElementById("mdp");
 
-    //Verif Mot De Passe
-    if(isset($_POST['MDP']) && isset($_POST['MDP_confirm'])){
-        $log = $_POST['Login'] ;
-        $mdp1 = $_POST['MDP'] ; 
-        $mdp2 = $_POST['MDP_confirm'] ; 
-        //Verification si l'utiliateur a remplis le champ MDP avec au moins 8 caracteres
-        if(!preg_match('/.{8,}/',$mdp1)){
-                $motDePasse = FALSE ;
-                $mdp_SizeError = TRUE ; 
-        }
-        elseif(!preg_match('/[A-Z]{1,}/',$mdp1)){
-            $mdp_UpperError = TRUE ;
-            $motDePasse = FALSE ;
-        }
-        elseif(!($mdp1 === $mdp2)){
-                $motDePasse = FALSE ; 
-                $mdp_EqualsError = TRUE ; 
+        if (!(/.{8,}/.test(mdp_input.value ))|| !/[A-Z]{1,}/.test(mdp_input.value) || !mdp_confirm_input.value === mdp_input.value ) {
+            if (!mdp.contains(label_mdp)) {
+                mdp.appendChild(label_mdp);
+            }
+            mdp.style.color = "red";
+        } else {
+            if (mdp.contains(label_mdp)) {
+                mdp.removeChild(label_mdp);
+            }
+            mdp.style.color = "black";
         }
     }
-    }  
+
+
+</script>
+<?php  
+session_start() ;
 ?>
-
     <h1>Inscription</h1>
+
     <form style="align-content: center;"  method="post" action="#" >
-        <span <?php if(!$login){ echo 'style="color: red;"';} ?> ><strong> Login </strong> 
-        <?php 
-        if($login_SizeError){ echo '<span style="color: red;font-size: 12px;"> (Erreur le login doit faire au moins 5 caractères)</span>';}
-        elseif ($login_ExistError) echo '<span style="color: red;font-size: 12px;"> (Erreur le login existe déjà)</span>';
-        ?>
-        <br>
-        <input type="text" name="Login"  > </span>
-        <br>
-        <span <?php if(!$motDePasse){ echo 'style="color: red;"';} ?>><strong>Mot de passe :</strong> 
-        <?php 
-        if($mdp_SizeError == TRUE){ echo '<span style="color: red;font-size: 12px;"> (Erreur le mot de passe doit faire au moins 8 caractères)</span>';
-        }elseif ($mdp_UpperError == TRUE) {  echo '<span style="color: red;font-size: 12px;"> (Erreur le mot de passe doit avoir au moins 1 majuscule)</span>';
-        }elseif($mdp_EqualsError == TRUE){ echo '<span style="color: red;font-size: 12px;"> (Erreur lors de la confirmation du mot de passe)</span>';
-        } 
-        ?>
-        <br> 
-        <input type="password" name="MDP"   > </span>
-        <br>
-        <span <?php if(!$motDePasse){ echo 'style="color: red;"';} ?>><strong>Confirmer Mot de passe : </strong>
-        <br>
-        <input type="password" name="MDP_confirm"   ></span>
-        <br>
-        <h3>Informations Optionnelles</h3>
-        <table>
-            <tr>
-                <td >
-                    <span <?php if(!$nom){ echo 'style="color: red;"';} ?>><strong> Nom </strong> </span> <?php if(!$nom){ echo '<span style="color: red;font-size: 12px;"> (Erreur Nom)</span>';} ?>
-                </td>
-                <td>
-                    <span <?php if(!$prenom){ echo 'style="color: red;"';} ?>><strong> Prenom </strong> </span> <?php if(!$prenom){ echo '<span style="color: red;font-size: 12px;"> (Erreur Prenom)</span>';} ?>
-                </td>
+    <?php  ?>
+    <h3>Information Optionel :</h3>
+    <label id="nom" >Nom :</label>
+  
+    <input id ="nom_input" type="text"  onblur="handleBlurNom()">
+    <label id="prenom" >Prenom :</label>
+    <input id ="prenom_input" type="text" onblur="handleBlurPrenom()" >
+    <br>
+    <label id="sexe" >Civilite :</label> 
+    <br>
+    <input type="radio" name="sexe" value="F">Femme
+    <input type="radio" name="sexe" value="H">Homme  
+    <br>
+    <label id="email" >Email :</label>
+    <input id ="email_input" type="email" onblur="handleBlurEmail()" >
+    <br>
+    <label id="tel" >Numero de telephone :</label>
+    <input id ="tel_input" type="text" maxlength="10" onblur="handleBlurTel()" >
+    <br>
+    <label id="ville" >Ville :</label>
+    <input id ="ville_input" type="text" onblur="handleBlurVille()" >
+    <br>
+    <label id="codePostal" >Code Postal :</label>
+    <input id ="codePostal_input" type="text" maxlength="5" onblur="handleBlurCodePostal()" >
+    <br>
+    <label id="adresse" >Adresse :</label>
+    <input id ="adresse_input" type="text" >
+    <br>
 
-            </tr>
-            <tr>
-                <td>
-                    <span><input type="text" name="Nom"  ></span>
-                </td>
-                <td>
-                    <span><input type="text" name="Prenom"  ></span>
-                </td>
-            </tr>
-        </table>
-        <span <?php if(!$sexe){ echo 'style="color: red;"';} ?>><strong>Sexe  :</strong><br> <input type="radio" name="sexe" value="F">Femme
-        <input type="radio" name="sexe" value="H">Homme  <br>
-    </span>
-        <span <?php if(!$email){ echo 'style="color: red;"';} ?>><strong>Email :</strong> <?php if(!$email){ echo '<span style="color: red;font-size: 12px;"> (Erreur EMAIL)</span>';} ?>
-        <br>
-         <input type="email" name="email"> </span>
-        <br>
-        <span <?php if(!$telephone){ echo 'style="color: red;"';} ?>><strong>Telephone :</strong> <?php if(!$telephone){ echo '<span style="color: red;font-size: 12px;"> (Erreur  numero de telephone)</span>';} ?>
-        <br> 
-        <input type="tel" name="telephone"> </span>
-        <br>
-        <span <?php if(!$naissance){ echo 'style="color: red;"';} ?>><strong>Date de naissance :</strong> <br> <input type="date" name="naissance" > </span>
-        <br>
-        <span ><strong>Adresse </strong><br> <input type="" name="adresse" > </span>
-        <br>
-        <span <?php if(!$ville){ echo 'style="color: red;"';} ?>><strong>Ville </strong> <?php if(!$ville){ echo '<span style="color: red;font-size: 12px;"> (Erreur Ville)</span>';} ?>
-        <br>
-        <input type="text" name="ville" > </span>
-        <br>
-        <span <?php if(!$codePostal){ echo 'style="color: red;"';} ?>><strong>Code postal </strong> <?php if(!$codePostal){ echo '<span style="color: red;font-size: 12px;"> (Erreur CodePostal)</span>';} ?>
-        <br>
-         <input type="text" name="Postal" maxlength="5" >  </span>
-        <br>
-        <input type="submit" name="Submit" value="Valider">
+    <h3 >Information Obligatoire : </h3>
+    <label id="login" >Login :</label>
+    <input id ="login_input" type="text" onblur="handleBlurLogin()" >
+    <br>
 
+    <label id="mdp" >Mot de passe :</label>
+    <input id ="mdp_input" type="text"  onblur="handleBlurMdp()">
+    <br>
+    <label id="mdp_confirm" > Confirmation Mot de passe :</label>
+    <input id ="mdp_confirm_input" type="text" onblur="handleBlurMdp()" >
+    <br>
+    <input type="submit" name="Submit" value="S'inscrire">
+
+    
+   
     </form>
 
 <?php  
